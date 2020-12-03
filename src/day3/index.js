7313bfaa7cbdd411ecbb65c067248262ceb6b3a9
albin <albinlandgreen@gmail.com>
2020-12-03 09:30:16 +0100

    Day 3

---
+++ src/day3/index.js
@@ -0,0 +1,38 @@
+const read = require('../utils/read');
+
+let matrix = read(__dirname).split('\r\n').map(x => x.split(''));
+
+const calcSlope = (down, across) => {
+	let coords = {
+		x: 0,
+		y: 0,
+	}
+	let treesForSlope = 0;
+	while (coords.y < matrix.length - 1) {
+		coords.x += across;
+		coords.y += down;
+		if (matrix[coords.y][coords.x % matrix[0].length] === '#') {
+			treesForSlope++;
+		}
+	}
+	return treesForSlope;
+}
+
+const A = () => calcSlope(1, 3);
+
+const B = () => {
+	let slopes = [
+		[1, 1],
+		[3, 1],
+		[5, 1],
+		[7, 1],
+		[1, 2],
+	];
+	let result = 1;
+	for (let slope of slopes) {
+		result *= calcSlope(slope[1], slope[0]);
+	}
+	return result;
+}
+console.log(A())
+console.log(B())
