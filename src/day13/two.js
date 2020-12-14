1d520be6d87706aaf01de17cdf86639283d1e73e
albin <albinlandgreen@gmail.com>
2020-12-14 14:11:34 +0100

    Day 13 final

--- src/day13/two.js
+++ src/day13/two.js
@@ -3,6 +3,9 @@ const { performance } = require('perf_hooks');
 
 let list = read(__dirname).split(/\n/g);
 
+const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
+const lcm = (a, b) => (a * b) / gcd(a, b);
+
 const buses = [];
 
 list[1]
@@ -13,11 +16,18 @@ const max = buses
 	.reduce((prev, curr) => (parseInt(prev[0]) > parseInt(curr[0]) ? prev : curr))
 
 let min = 100000000000000
-let t = Math.floor(200 / max[0]) * max[0];
+let t = Math.floor(min / max[0]) * max[0];
 let [maxBus, maxIndex] = max;
-while (true) {
-	if (buses.every(bus => (t - maxIndex + bus[1]) % bus[0] === 0)) break;
-	t += maxBus;
+let m = maxBus;
+let t1 = performance.now();
+for (let i = 0; i < buses.length; i++) {
+	const routes = buses.slice(0, i + 1);
+	while (!routes.every(([bus, index]) => (t - maxIndex + index) % bus === 0)) {
+		t += m;
+	}
+	m = routes.reduce((m, [bus]) => lcm(m, bus), m);
 }
 
-console.log(t - max[1])
+let t2 = performance.now()
+
+console.log(t - max[1], t2 - t1)
